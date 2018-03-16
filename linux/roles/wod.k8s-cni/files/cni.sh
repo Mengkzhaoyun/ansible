4ae97be53017334df8f1719c9428a258272eb6fd
shucheng <shucheng@spacesystech.com>
2018-03-17 00:05:59 +0800

    update

---
+++ linux/roles/wod.k8s-cni/files/cni.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+set -e  
+
+HTTP_SERVER="${HTTP_SERVER:-https://gitlab.ispacesys.cn/source/cig/raw/master/k8s}"
+K8S_CNI_BIN="${K8S_CNI_BIN:-cni-plugins-amd64-v0.7.0}"
+
+mkdir -p /opt/cni/bin
+
+if [[ -e /opt/cni/bin/host-local ]]; then
+  echo 'cni bin is already exist!'
+else
+  curl $HTTP_SERVER/$K8S_CNI_BIN.tgz > /opt/cni/bin/$K8S_CNI_BIN.tgz
+  cd /opt/cni/bin && tar -xzf /opt/cni/bin/$K8S_CNI_BIN.tgz
+  rm -rf /opt/cni/bin/$K8S_CNI_BIN.tgz
+  echo 'cni bin download completed!'
+fi
