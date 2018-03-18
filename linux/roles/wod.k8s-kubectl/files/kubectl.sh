175b3d17e6fa1df72eeefd3731b85b155e0e62c6
shucheng <shucheng@spacesystech.com>
2018-03-18 11:25:09 +0800

    update

--- linux/roles/wod.k8s-kubectl/files/kubectl.sh
+++ linux/roles/wod.k8s-kubectl/files/kubectl.sh
@@ -20,8 +20,7 @@ if ! [ -x "$(command -v kubectl)" ]; then
   fi
   chmod 0744 /etc/kubernetes/downloads/$DEP_KUBECTL
   ln -s /etc/kubernetes/downloads/$DEP_KUBECTL /opt/bin/kubectl
-  /opt/bin/kubectl config set-cluster kubernetes --server=https://$MASTER_SERVER --certificate-authority=$CA_CERT --embed-certs=true
-  /opt/bin/kubectl config set-credentials admin --certificate-authority=$CA_CERT --client-key=$ADMIN_KEY --client-certificate=$ADMIN_CERT --embed-certs=true
-  /opt/bin/kubectl config set-context kubernetes --cluster=kubernetes --user=admin
+  /opt/bin/kubectl config set-cluster kubernetes --server=http://127.0.0.1:8080
+  /opt/bin/kubectl config set-context kubernetes --cluster=kubernetes 
   /opt/bin/kubectl config use-context kubernetes
 fi
